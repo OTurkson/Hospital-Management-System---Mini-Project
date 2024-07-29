ab39fc3b6debc340ce7454aa836512cca5401c7c
Owusu-Turkson <oteekow@gmail.com>
2024-07-29 00:03:42 +0000

    included swagger documentation

--- HMS/urls.py
+++ HMS/urls.py
@@ -16,10 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hospital Management API",
+      default_version='v1',
+      description="Documentation for HMS",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="trxpkvng100@gamil.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('patients/', include('patients.urls')),
     path('staff/', include('staff.urls')),
     path('', include('accounts.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
